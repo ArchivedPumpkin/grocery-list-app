4d44a3b420a04cf32e94ba4444b08c75961bca99
ArchivedPumpkin <106057361+ArchivedPumpkin@users.noreply.github.com>
2025-06-06 15:20:58 +0200

    Initial commit Grocery List App

---
+++ config.example.js
@@ -0,0 +1,3 @@
+export const FIREBASE_DATABASE_URL = "<YOUR_FIREBASE_DATABASE_URL>"; // Replace with your Firebase Realtime Database URL
+
+export const APP_CHECK_RECAPTCHA_SITE_KEY = "<YOUR_RECAPTCHA_SITE_KEY>"; // Replace with your reCAPTCHA site key
---
+++ index.js
@@ -0,0 +1,68 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+import {
+  initializeAppCheck,
+  ReCaptchaV3Provider,
+} from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app-check.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/11.8.1/firebase-database.js";
+
+import {
+  FIREBASE_DATABASE_URL,
+  APP_CHECK_RECAPTCHA_SITE_KEY,
+} from "./config.js";
+
+const firebaseConfig = {
+  databaseURL: FIREBASE_DATABASE_URL,
+}; // Your Firebase configuration object
+
+const app = initializeApp(firebaseConfig);
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider(APP_CHECK_RECAPTCHA_SITE_KEY),
+  isTokenAutoRefreshEnabled: true, // Set to true to enable automatic token refresh
+});
+
+const database = getDatabase(app);
+const referenceInDb = ref(database, "groceries");
+
+console.log(app);
+
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+
+function render(groceries) {
+  let listItems = "";
+  for (let i = 0; i < groceries.length; i++) {
+    listItems += `
+            <li>${groceries[i]}</li>
+        `;
+  }
+  ulEl.innerHTML = listItems;
+}
+
+deleteBtn.addEventListener("dblclick", function () {
+  remove(referenceInDb);
+  ulEl.innerHTML = "";
+});
+
+onValue(referenceInDb, function (snapshot) {
+  const snapshotExists = snapshot.exists();
+
+  if (snapshotExists) {
+    const snapshotValues = snapshot.val();
+    const groceries = Object.values(snapshotValues);
+    console.log(groceries);
+    render(groceries);
+  }
+});
+
+inputBtn.addEventListener("click", function () {
+  push(referenceInDb, inputEl.value);
+  inputEl.value = "";
+});
