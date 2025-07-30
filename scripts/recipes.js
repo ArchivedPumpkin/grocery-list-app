afe14c0ee66744737749f9e101b6abcf2f62b410
ArchivedPumpkin <106057361+ArchivedPumpkin@users.noreply.github.com>
2025-07-30 19:46:37 +0200

    Added functionality to create and add new recipes

--- scripts/recipes.js
+++ scripts/recipes.js
@@ -17,3 +17,44 @@ if (location.hostname === "localhost") {
     auth = getAuth(app);
     db = getDatabase(app);
 }
+
+onAuthStateChanged(auth, async (user) => {
+    if (!user) {
+        window.location.href = "/pages/auth.html";
+        return;
+    }
+
+    console.log("User is authenticated:", user.uid);
+
+    const userNameRef = ref(db, `users/${user.uid}/username`);
+    const userNameSnapshot = await get(userNameRef);
+    const userName = userNameSnapshot.exists() ? userNameSnapshot.val() : "Guest";
+
+    const addRecipeBtn = document.getElementById("new-recipe-btn");
+    const recipesList = document.getElementById("recipes-list");
+
+    addRecipeBtn.addEventListener("click", async () => {
+        const recipeName = prompt("Enter the name of the new recipe:");
+        if (recipeName) {
+            const newRecipeRef = push(ref(db, "groceryLists/recipes"));
+
+            try {
+                await set(newRecipeRef, {
+                    name: recipeName,
+                    ingredients: {},
+                    instructions: "",
+                    createdAt: new Date(Date.now()).toLocaleDateString(),
+                    createdBy: {
+                        id: user.uid,
+                        name: userName
+                    }
+                })
+
+                console.log("Recipe added successfully:", recipeName);
+            } catch (error) {
+                console.error("Error adding recipe:", error);
+            }
+        }
+    })
+
+});
