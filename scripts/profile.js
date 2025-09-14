0ddd8e971c8af3f6f8cc50c901a3ca94ef5ba99e
ArchivedPumpkin <106057361+ArchivedPumpkin@users.noreply.github.com>
2025-09-14 23:38:32 +0200

    Add loading indicator and shared list management features in profile section
    - Implement loading animation for shared lists.
    - Refactor shared list display logic to better handle memers not being part of any shared lists.

--- scripts/profile.js
+++ scripts/profile.js
@@ -24,6 +24,11 @@ const friendsList = document.getElementById("friends-list");
 const logoutBtn = document.getElementById("logout-btn");
 const editBtn = document.getElementById("edit-list-btn");
 
+friendsList.innerHTML = `
+    <div class="loading-container">
+        <div class="loader"></div>
+    </div>`;
+
 onAuthStateChanged(auth, (user) => {
     if (!user) {
         window.location.href = "/pages/auth.html";
@@ -31,6 +36,8 @@ onAuthStateChanged(auth, (user) => {
     }
 
     console.log("User is authenticated:", user.uid);
+    displayUserProfile();
+
 
     logoutBtn.addEventListener("click", async () => {
         console.log("Logging out...");
@@ -45,117 +52,131 @@ onAuthStateChanged(auth, (user) => {
 
     // Fetch and display user profile information
     async function displayUserProfile() {
-        const userRef = ref(db, `users/${user.uid}`)
-        const userSnapshot = await get(userRef);
+        try {
+            const userRef = ref(db, `users/${user.uid}`)
+            const userSnapshot = await get(userRef);
 
-        if (userSnapshot.exists()) {
-            const userData = userSnapshot.val();
-            console.log("User data:", userData);
+            friendsList.innerHTML = ""; // Clear loading indicator
 
-            usernameEl.textContent = userData.username;
-            handleEl.textContent = `#${user.uid.slice(0, 5)}`; // Display first 5 characters of username id as handle
+            if (userSnapshot.exists()) {
+                const userData = userSnapshot.val();
+                console.log("User data:", userData);
 
-            // Display shared lists if any
-            const sharedListsRef = ref(db, `groceryLists/lists`)
-            const sharedListsSnapshot = await get(sharedListsRef);
+                usernameEl.textContent = userData.username;
+                handleEl.textContent = `#${user.uid.slice(0, 5)}`; // Display first 5 characters of username id as handle
 
-            if (sharedListsSnapshot.exists()) {
-                const sharedLists = sharedListsSnapshot.val();
-                console.log("Shared lists:", sharedLists);
+                // Display shared lists if any
+                const sharedListsRef = ref(db, `groceryLists/lists`)
+                const sharedListsSnapshot = await get(sharedListsRef);
+                let hasSharedLists = false;
 
-                for (let sharedId in sharedLists) {
-                    const listData = sharedLists[sharedId];
+                if (sharedListsSnapshot.exists()) {
+                    const sharedLists = sharedListsSnapshot.val();
+                    console.log("Shared lists:", sharedLists);
 
-                    const membersList = Object.values(listData.members).map(member => member.username).join(', ');
-                    console.log("Members in shared list:", membersList);
+                    for (let sharedId in sharedLists) {
+                        const listData = sharedLists[sharedId];
 
-                    const listItem = document.createElement("li");
-                    if (!listData.members || !listData.members[user.uid]) {
-                        continue; // Skip lists where the user is not a member
-                    }
-                    listItem.innerHTML = `
-                    
-                    <div class="list-item">
-                    <a href="/pages/list.html?list=${sharedId}" class="view-list-btn">
-                        <div class="list-details">
-                            <p id="list-name">${listData.name}</p>
-                            <p id="list-members">${membersList}</p>
-                        </div>
-                        
-                        <i class="fa-solid fa-angle-right view-list" id="view-list-btn" aria-hidden="true"></i>
-                        </a>
-                         <i class="fa-solid fa-trash delete-list-btn" style="display: none;" aria-hidden="true"></i>
-                    </div>`;
-                    friendsList.appendChild(listItem);
+                        if (listData.members && listData.members[user.uid]) {
+                            hasSharedLists = true;
+                            const membersList = Object.values(listData.members).map(member => member.username).join(', ');
+                            console.log("Members in shared list:", membersList);
 
-                    const deleteBtn = listItem.querySelector(".delete-list-btn");
-                    const viewBtn = listItem.querySelector(".view-list-btn");
-                    const viewIcon = listItem.querySelector("#view-list-btn");
-                    let isEditing = false;
-
-                    editBtn.addEventListener("click", () => {
-                        isEditing = !isEditing;
-                        console.log("Toggling edit mode:", isEditing);
-                        if (isEditing) {
-                            viewBtn.style.pointerEvents = "none"; // Disable link
-                            deleteBtn.style.display = "block";
-                            viewIcon.style.display = "none";
-                            editBtn.textContent = "Done";
-                        } else {
-                            viewBtn.style.pointerEvents = "auto"; // Enable link
-                            deleteBtn.style.display = "none";
-                            viewIcon.style.display = "block";
-                            editBtn.textContent = "Edit";
+                            const listItem = document.createElement("li");
+                            listItem.innerHTML = `
+                            <div class="list-item">
+                            <a href="/pages/list.html?list=${sharedId}" class="view-list-btn">
+                                <div class="list-details">
+                                    <p id="list-name">${listData.name}</p>
+                                    <p id="list-members">${membersList}</p>
+                                </div>
+                                
+                                <i class="fa-solid fa-angle-right view-list" id="view-list-btn" aria-hidden="true"></i>
+                                </a>
+                                <i class="fa-solid fa-trash delete-list-btn" style="display: none;" aria-hidden="true"></i>
+                            </div>`;
+                            friendsList.appendChild(listItem);
                         }
-                    })
 
-                    deleteBtn.addEventListener("click", async () => {
-                        try {
-                            const listRef = ref(db, `groceryLists/lists/${sharedId}`);
-                            const userListRef = ref(db, `users/${user.uid}/groceryLists/lists/${sharedId}`);
-                            const sharedListRef = ref(db, `users/${user.uid}/groceryLists/sharedLists/${sharedId}`);
-                            const listSnapshot = await get(listRef);
-                            const userListSnapshot = await get(userListRef);
-                            const sharedListSnapshot = await get(sharedListRef);
+                        const deleteBtn = listItem.querySelector(".delete-list-btn");
+                        const viewBtn = listItem.querySelector(".view-list-btn");
+                        const viewIcon = listItem.querySelector("#view-list-btn");
+                        let isEditing = false;
 
-                            if (listSnapshot.exists()) {
-                                const listData = listSnapshot.val();
-                                const memberCount = listData.members ? Object.keys(listData.members).length : 0;
-                                console.log("Member count:", memberCount);
+                        editBtn.addEventListener("click", () => {
+                            isEditing = !isEditing;
+                            console.log("Toggling edit mode:", isEditing);
+                            if (isEditing) {
+                                viewBtn.style.pointerEvents = "none"; // Disable link
+                                deleteBtn.style.display = "block";
+                                viewIcon.style.display = "none";
+                                editBtn.textContent = "Done";
+                            } else {
+                                viewBtn.style.pointerEvents = "auto"; // Enable link
+                                deleteBtn.style.display = "none";
+                                viewIcon.style.display = "block";
+                                editBtn.textContent = "Edit";
+                            }
+                        })
 
-                                if (memberCount <= 1) {
-                                    await set(listRef, null);
-                                    console.log("List deleted as user was the last member.");
-                                } else {
-                                    const memberRef = ref(db, `groceryLists/lists/${sharedId}/members/${user.uid}`);
-                                    await set(memberRef, null);
-                                    console.log("User removed from shared list members.");
+                        deleteBtn.addEventListener("click", async () => {
+                            try {
+                                const listRef = ref(db, `groceryLists/lists/${sharedId}`);
+                                const userListRef = ref(db, `users/${user.uid}/groceryLists/lists/${sharedId}`);
+                                const sharedListRef = ref(db, `users/${user.uid}/groceryLists/sharedLists/${sharedId}`);
+                                const listSnapshot = await get(listRef);
+                                const userListSnapshot = await get(userListRef);
+                                const sharedListSnapshot = await get(sharedListRef);
+
+                                if (listSnapshot.exists()) {
+                                    const listData = listSnapshot.val();
+                                    const memberCount = listData.members ? Object.keys(listData.members).length : 0;
+                                    console.log("Member count:", memberCount);
+
+                                    if (memberCount <= 1) {
+                                        await set(listRef, null);
+                                        console.log("List deleted as user was the last member.");
+                                    } else {
+                                        const memberRef = ref(db, `groceryLists/lists/${sharedId}/members/${user.uid}`);
+                                        await set(memberRef, null);
+                                        console.log("User removed from shared list members.");
+                                    }
+                                    listItem.remove();
                                 }
-                                listItem.remove();
+
+                                if (userListSnapshot.exists()) {
+                                    await set(userListRef, null);
+                                    console.log("List reference removed from user's profile.");
+                                }
+
+                                if (sharedListSnapshot.exists()) {
+                                    await set(sharedListRef, null);
+                                    console.log("Shared list reference removed from user's profile.");
+                                }
+
+                            } catch (error) {
+                                console.error("Error removing user from shared list:", error);
                             }
+                        })
 
-                            if (userListSnapshot.exists()) {
-                                await set(userListRef, null);
-                                console.log("List reference removed from user's profile.");
-                            }
+                    }
+                }
 
-                            if (sharedListSnapshot.exists()) {
-                                await set(sharedListRef, null);
-                                console.log("Shared list reference removed from user's profile.");
-                            }
-
-                        } catch (error) {
-                            console.error("Error removing user from shared list:", error);
-                        }
-                    })
-
+                if (!hasSharedLists) {
+                    const listItem = document.createElement("li");
+                    listItem.innerHTML = `
+                <div class="list-item">
+                    <p>You are not a member of any shared lists.</p>
+                </div>`
+                    friendsList.appendChild(listItem);
                 }
             }
-
-
-
+        } catch (error) {
+            console.error("Error loading profile:", error);
+            friendsList.innerHTML = `
+            <div class="list-item">
+                <p>Error loading lists. Please try again.</p>
+            </div>`;
         }
     }
-
-    displayUserProfile()
-})
+});
